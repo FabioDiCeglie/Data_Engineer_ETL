05f3b380d78477a29ad04be0c01cf8d02e580118
FabioDiCeglie <dcfabio96@gmail.com>
2023-02-20 19:24:10 +0100

    feat: added environ

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ import json
 from datetime import datetime
 import datetime
 import sqlite3
+import environ
 
-DATABASE_LOCATION="sqlite:///my_played_tracks.sqlite"
-USER_ID="Fabiuc"
-TOKEN="BQB3XCKHjBysf7oUIuJvbkn4zLwFg7_JZfXU6bjIPsl7XL1uX4T40Io68A-KbyKt_dQg6lo47CzioqYe1xZYyY23bjM9z4hi-D4diDyv4RwJBjnn_pBzDVO8B5TbU0MaDHWV3DIPP3WM6jcQj_qwzAhmJgLECXLzeh9rYa7HnI9jezKzk9ZXjEnhUpbqFJG3FkUYoDyAVsM9CvbDnWErIB59FVrDsdyivMlyIwVPfebOxJ7uNutbDpQDnQMpb64ZTXyiG3AD-fU2dXffS4T99KDTY_yUULnIGZyED8MWKcZliNFxBw-7s0eKEaOB1OW3W7erAM4dibahjg"
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
+DATABASE_LOCATION=env('DATABASE_LOCATION')
+USER_ID=env('USER_ID')
+TOKEN=env('TOKEN')
 
 if __name__ == "__main__":
     headers={
@@ -20,9 +25,7 @@ if __name__ == "__main__":
 
     today = datetime.datetime.now()
     yesterday = today - datetime.timedelta(days=1)
-    print(yesterday)
     yesterday_unix_timestamp= int(yesterday.timestamp()) * 1000
-    print(yesterday_unix_timestamp)
 
     r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers = headers)
 
