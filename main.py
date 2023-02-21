d8a4146eb25987e63c18ce15d520822e500ef76a
FabioDiCeglie <dcfabio96@gmail.com>
2023-02-21 21:33:02 +0100

    feat: add load step with sql

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import sqlalchemy
 import pandas as pd
 from sqlalchemy.orm import sessionmaker
 import requests
-import json
 from datetime import datetime, timedelta
 import sqlite3
 import environ
-import time
 
 # Initialise environment variables
 env = environ.Env()
@@ -89,3 +87,29 @@ if __name__ == "__main__":
     # Validate
     if check_if_valid_data(song_df):
         print("Data valid, proceed to Load stage")
+
+    # Load
+
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    conn = sqlite3.connect('my_played_tracks.sqlite')
+    cursor = conn.cursor()
+
+    sql_query = """
+        CREATE TABLE IF NOT EXISTS my_played_tracks(
+        song_name VARCHAR(200),
+        artist_name VARCHAR(200),
+        played_at VARCHAR(200),
+        timestamp VARCHAR(200),
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+    )
+    """
+    cursor.execute(sql_query)
+    print("Opened database successfully")
+
+    try:
+        song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
+    except:
+        print("Data already exists in the database")
+
+    conn.close()
+    print("Close database successfully")
